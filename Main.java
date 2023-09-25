b2bef83d2d65216ff136ab5cf764b154da911527
PersianFlacc <129433476+PersianFlacc@users.noreply.github.com>
2023-09-25 22:09:50 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,44 +6,90 @@ class Main {
 
         System.out.println("Hello my Dear friend! It's your pocket calculator!");
 
-        System.out.println("To get started, enter button 1");
+        System.out.println("Choose what you want to do with these numbers" +
+                "\n1 - (a+b)" +
+                "\n2 - (a-b)" +
+                "\n3 - (a*b)" +
+                "\n4 - (a/b)" +
+                "\n5 - (a%b)" +
+                "\n6 - (sqrt(x)" +
+                "\n7 - (pow(x,y))");
 
-        Scanner sc = new Scanner(System.in);
-        int button = sc.nextInt();
-
-        System.out.println("Input your first number: ");
-
-        int n1 = sc.nextInt();
-
-        System.out.println("Input your second number ");
-        
-        int n2 = sc.nextInt();
-
-        System.out.println("Choose what you want to do with these numbers ");
-        System.out.println("1 - (a+b)");
-        System.out.println("2 - (a-b)");
-        System.out.println("3 - (a*b)");
-        System.out.println("4 - (a/b)");
-        System.out.println("5 - (a%b)");
+        Scanner sc = new Scanner (System.in);
 
         int knopka = sc.nextInt();
 
         switch (knopka) {
             case 1:
-                System.out.println("Your numbers: " + (n1+n2));
+
+                System.out.println("Input first number: ");
+                int a1 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int a2 = sc.nextInt();
+
+                System.out.println("Your answer: " + (a1+a2));
                 break;
             case 2:
-                System.out.println("Your numbers: " + (n1-n2));
+
+                System.out.println("Input first number: ");
+                int b1 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int b2 = sc.nextInt();
+
+                System.out.println("Your answer: " + (b1-b2));
                 break;
             case 3:
-                System.out.println("Your numbers: " + (n1*n2));
+
+                System.out.println("Input first number: ");
+                int c1 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int c2 = sc.nextInt();
+
+                System.out.println("Your answer: " + (c1*c2));
                 break;
             case 4:
-                System.out.println("Your numbers: " + (n1/n2));
+
+                System.out.println("Input first number: ");
+                int d1 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int d2 = sc.nextInt();
+
+                if (d2 == 0) {
+                    System.out.println("ERROR! You can't divide by 0!");
+                }else{
+                    System.out.println("Your answer: " + (d1 / d2));
+                }
                 break;
             case 5:
-                System.out.println("Your numbers: " + (n1%n2));
+
+                System.out.println("Input first number: ");
+                int d3 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int d4 = sc.nextInt();
+
+                System.out.println("Your answer: " + (d3 % d4));
                 break;
+            case 6:
+
+                System.out.println("Input your number: ");
+                int e = sc.nextInt();
+
+                System.out.println("Your answer: " + Math.sqrt(e));
+                break;
+            case 7:
+
+                System.out.println("Input first number: ");
+                int f1 = sc.nextInt();
+
+                System.out.println("Input second number: ");
+                int f2 = sc.nextInt();
+
+                System.out.println("Your answer: " + Math.pow(f1, f2));
         }
 
         sc.close();
